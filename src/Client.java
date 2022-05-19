60ff02131498d0a8fd5ecc1105c366338bea10da
HossamElkordi <hossam_elkordi@yahoo.com>
2022-05-19 22:26:42 +0200

    Random Queries

--- src/Client.java
+++ src/Client.java
@@ -6,18 +6,26 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
 
     private File logFile;
+    private List<String> addRemove, query;
+    private Random rand;
     public Client(String ip, String qFile, String logfile) throws RemoteException, NotBoundException {
         try{
             Registry registry = LocateRegistry.getRegistry(ip);
             GraphInterface stub = (GraphInterface) registry.lookup("Update");
             logFile = new File(logfile);
-            String queries = this.readQueries(qFile);
+            addRemove = this.readCommands("AddRemove");
+            query = this.readCommands("Query");
+            rand = new Random(1);
+            String queries = this.readQueries(0.6f);
             String res = stub.executeQuery(queries);
             this.logResults(res);
             // Sleep, choose a random amount first
@@ -29,18 +37,35 @@ public class Client {
         }
     }
 
-    private String readQueries(String path) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File(path));
+    private String readQueries(float percent) {
         StringBuilder queries = new StringBuilder();
-        String line = scan.nextLine();
-        while(true){
-            queries.append(line);
-            line = scan.nextLine();
-            if(line.equals("F")) break;
+        int size = rand.nextInt(5) + 4, arSize = (int)(percent * size);
+        String[] idx = new String[size];
+        int i = 0;
+        while(arSize >= 0){
+            i = rand.nextInt(size);
+            if(idx[i].isEmpty()){
+                idx[i] = this.addRemove.get(rand.nextInt(this.addRemove.size()));
+                --arSize;
+            }
+        }
+        for(int j = 0; j < size; ++j)
+            if(idx[j].isEmpty()) idx[j] = this.query.get(rand.nextInt(this.query.size()));
+
+        for(String s : idx){
+            queries.append(s);
             queries.append("\n");
         }
-        scan.close();
+        queries.append("F");
         return queries.toString();
+    }
+
+    private List<String> readCommands(String commandType) throws FileNotFoundException {
+        List<String> list = new ArrayList<String>();
+        Scanner scan = new Scanner(new File(commandType));
+        while(scan.hasNextLine()) list.add(scan.nextLine());
+        scan.close();
+        return list;
     }
 
     private void logResults(String res) throws IOException {
